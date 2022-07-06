b387d9a954baa520853ecd0c9aa6b9c8a2feabdd
Adetwelve <niyero10@gmail.com>
2022-07-06 06:42:47 -0700

    abc x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ *
+ * print_alphabet_x10 - prints lowercase alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char w, v;
+	for (v = 0; v <= 9; v++)
+	{
+		for (w = 'a'; w <= 'w'; w++)
+		{
+			_putchar(w);
+		
+		}
+	   
+		_putchar('\n');
+	
+	
+	}
+
+
+
+}
