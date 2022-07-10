72dc51c86b46bafa33a7aeb6e522cfa741f5177b
Adetwelve <niyero10@gmail.com>
2022-07-09 20:45:36 -0700

    print most number from 0-9 except 2,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print except
+ */
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		if (j != 2 && j != 4)
+		_putchar(j + '0');
+	}
+	_putchar('\n');
+}
