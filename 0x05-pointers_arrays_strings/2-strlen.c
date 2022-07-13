a7d0ba8a9120eb4b0e3e981b1df0bda0e2c2eaee
Adetwelve <niyero10@gmail.com>
2022-07-12 19:11:37 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - func get length of string
+ *@s: string pointer to passed to this function
+ *Return: return length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
