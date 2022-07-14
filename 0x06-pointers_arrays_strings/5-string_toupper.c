4141a02c1b5b6f54ffabd76f85055b10c5696038
Adetwelve <niyero10@gmail.com>
2022-07-14 11:40:05 -0700

    lower case to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *string_toupper - converting any string to upper case
+ *@x : string parameter 
+ *Return: string
+ */
+char *string_toupper(char *x)
+{
+	int j = 0;
+	
+	while (x[j])
+	{
+		if (x[j] >= 97 && x[j] <= 122)
+			x[j] = x[j] - 32;
+		j++;
+	}
+	return (x);
+}
