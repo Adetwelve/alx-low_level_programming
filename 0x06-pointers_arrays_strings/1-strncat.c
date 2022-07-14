a46e72ab95a6706f024d841183974b36cb0a8d07
Adetwelve <niyero10@gmail.com>
2022-07-14 09:47:28 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  *_strncat - function append src to dest
@@ -9,22 +10,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, b;
-
-	a = b = 0;
-	while (*(dest + a))
-		a++;
-
-		while (b < n && *(src + b))
-
-		{
-			*(dest + a) = *(src + b);
-			a++;
-			b++;
-		}
-
-	if (b < n)
-		*(dest + a) = *(src + b);
+	strncat(dest, src, n);
 	return (dest);
 }
 
