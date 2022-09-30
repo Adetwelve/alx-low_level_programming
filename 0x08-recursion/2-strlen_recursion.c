c382f1c80ff50f2edc0d31efcfbfe88a04446aad
Adetwelve <niyero10@gmail.com>
2022-09-29 19:56:47 -0700

    length of string recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,11 +4,13 @@
  *_strlen_recursion - print length of string recusively
  *
  *@s: parameter
+ *
+ * Return: the length of string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
 
-	return(_strlen_recursion (++s) + 1);
+	return (_strlen_recursion(++s) + 1);
 }
