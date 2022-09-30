aeeb5a67d06bf271a22c5bd54234726f3e11080d
Adetwelve <niyero10@gmail.com>
2022-09-29 20:46:09 -0700

    square root recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int get_sqrt(int n, possible_root);
+
+/**
+ *_sqrt_recursion - return natural square root of a number.
+ *
+ *@n: parameter
+ *
+ * Return: returns the natural square root of anumber.
+ */
+int _sqrt_recursion(int n)
+{
+	return (get_sqrt(n, 1));
+}
+
+/**
+ *get_sqrt - tries to get square root of n by continously
+ *comparing the square of possible root to n
+ *
+ *@n: integer to find sqrt of
+ *@p: possible root of n
+ *
+ *Return: natural square root or -1
+ */
+int get_sqrt(int n, int p)
+{
+	int square = p * p;
+
+	if (square == n)
+		return (p);
+	if (square < n)
+		return (get_sqrt(n, ++p));
+
+	return (-1);
+}
