777182130481a8572afc90f2849c5528150dc04d
Adetwelve <niyero10@gmail.com>
2022-10-02 07:39:30 -0700

    print name on new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print it name followed by new line.
+ *
+ *@argc: count parameter
+ *@argv: parameter
+ *
+ *Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
