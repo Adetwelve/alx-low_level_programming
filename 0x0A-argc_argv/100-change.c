c0c5dc4e5a74ea171a4f4602575f9b2cb80eec2b
Adetwelve <niyero10@gmail.com>
2022-10-03 08:48:42 -0700

    coin change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print mininimum number of coins to make change
+ *
+ *@argc: parameter count
+ *@argv: parameter
+ *
+ *Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int b = 0, d;
+	int a;
+	unsigned i;
+	int cent[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			d = a / cent[i];
+			a -= d * cent[i];
+			b += d;
+			if (a == 0)
+				break;
+		}
+
+	}
+	printf("%d\n", b);
+	return (0);
+}
