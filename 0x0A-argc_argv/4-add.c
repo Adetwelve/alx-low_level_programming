33db53e283bdcec4ed5dafeaeae844e74f2515e0
Adetwelve <niyero10@gmail.com>
2022-10-03 03:46:20 -0700

    add integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds 2 positive integer.
+ *
+ *@argc: parameter count.
+ *@argv: parameter
+ *
+ *Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+
+}
