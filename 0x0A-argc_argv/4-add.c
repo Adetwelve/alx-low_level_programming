83022583e0f7ce96a7bbf66202a544dc2d7130e9
Adetwelve <niyero10@gmail.com>
2022-10-03 05:31:32 -0700

    requirement fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,17 +13,16 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	/*
-	*if (argc == 0)
-	*{
-	*	return (0);
-	*}
-	*/
+	if (argc < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		if (!atoi(argv[i]))
 		{
-			printf("%s\n", "Error");
+			printf("Error\n");
 			return (1);
 		}
 		else
