77ac9ab6d3f2f9cd0954b6657696a13b281ada87
Adetwelve <niyero10@gmail.com>
2022-10-03 00:55:07 -0700

    number of argumment

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - print number of argumment.
+ *
+ *@argc: parameter for count
+ *@argv: parameter
+ *
+ *Return: always 0 on success
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
