522c201ca69c1eff22f77cc8f328b7925795467b
Adetwelve <niyero10@gmail.com>
2022-10-14 03:37:53 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *struct dog - structure defination of dog
+ *@name: string parameter
+ *@age: integer parameter
+ *@owner string parameter
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
