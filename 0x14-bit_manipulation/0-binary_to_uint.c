a290996e573d7c48ed9d785c095517d84e8f49ef
Adetwelve <niyero10@gmail.com>
2022-10-28 02:37:44 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *binary_to_unit - converts a binary to int
+ *@s: pointer to buinary chars
+ *Return: int value or zero
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0, i;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		res = res << 1;
+		res |= (b[i] - '0');
+	}
+	return (res);
+}
