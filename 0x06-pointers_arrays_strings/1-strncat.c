ae668f748458385d07eb3879a20a87a49c0f3777
Adetwelve <niyero10@gmail.com>
2022-10-29 19:55:40 -0700

    append src n number of times

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,20 +5,20 @@
  *@dest: destination parameter
  *@src: source of string to append
  *@n: number of charaters in string to be appended
- *Return 0 on success
+ *Return: 0 on success
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	 int i, len1 = 0, len2 = 0;
 
-	 while (dest[len1])
-	 	len1++;
+	while (dest[len1])
+		len1++;
 
-	 while (src[len2])
-		 len2++;
+	while (src[len2])
+		len2++;
 
-	 for (i = 0; i < n && i < len2; i++)
-		 dest[len1 + i] = src[i];
+	for (i = 0; i < n && i < len2; i++)
+		dest[len1 + i] = src[i];
 
-	 return (dest);
+	return (dest);
 }
