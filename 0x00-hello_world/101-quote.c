c43026bdb6a6255dccb6b23d9e9e730a417cc8b0
Adetwelve <niyero10@gmail.com>
2023-03-02 03:25:27 +0000

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quo, sizeof(quote) - 1);
+	write(2, quo, sizeof(quo) - 1);
 	return (1);
 }
